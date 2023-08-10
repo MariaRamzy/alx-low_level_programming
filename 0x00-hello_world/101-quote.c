3083aab60d4f3cb2c5993d58582e7282a642e273
MariaRamzy <maria_botros94@hotmail.com>
2023-08-10 12:03:00 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main -  prints exact and that piece of art is useful Dora Korpar,
  *
  * Return: return 1 (Success) program stopped
  */
-int write(void);
 int main(void)
 {
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
