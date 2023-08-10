b8f4cbf41c70a8decf11905caf8eaf3e64ea5d5d
MariaRamzy <maria_botros94@hotmail.com>
2023-08-10 11:44:48 -0700

    no printf/puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,9 @@
  *
  * Return: return 1 (Success) program stopped
  */
- 
-
 int write(int fieldes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-
         write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
         return (1);
 }
