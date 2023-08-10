0231a195913b54d9b3c95c387874960886140fd1
MariaRamzy <maria_botros94@hotmail.com>
2023-08-10 04:42:05 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - main block
  * return - success and the program stopped
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
