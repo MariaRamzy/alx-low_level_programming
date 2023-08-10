be1b97b4abdfa7f318589354b0a5ca5c0ec5a307
MariaRamzy <maria_botros94@hotmail.com>
2023-08-10 04:27:45 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - main block
- * return: 0
+ * return: 0 success
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
