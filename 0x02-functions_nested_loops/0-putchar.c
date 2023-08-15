c4e15b8e33f9f1a2aa65fb5e0e0a612807a0156e
MariaRamzy <maria_botros94@hotmail.com>
2023-08-15 00:31:44 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - prints _putchar
+ *
+ * Return: Always return 0 (Success) program stopped
+ */
+int main(void)
+{
+char c[9] = "_putchar";
+int i;
+for(i=0; i < 8; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
+}
