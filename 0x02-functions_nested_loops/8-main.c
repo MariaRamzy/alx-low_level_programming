30b96f0a3f72ece9ef685cc41cdbf1ec9347b211
MariaRamzy <maria_botros94@hotmail.com>
2023-08-15 08:48:37 -0700

    minute of day

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print every minute of day
+ *
+ * Return: Always return 0 (Success) program stopped
+ */
+int main(void)
+{
+jack_bauer();
+return (0);
+}
