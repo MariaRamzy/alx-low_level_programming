d14c5b20f5146ba0880850e9f10d344df8e5fe13
MariaRamzy <maria_botros94@hotmail.com>
2023-08-15 09:04:58 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print time table
+ *
+ * Return: Always return 0 (Success) program stopped
+ */
+int main(void)
+{
+times_table();
+return (0);
+}
