72850ba0c884ad04ab3cb9f84eaf61541b17c764
MariaRamzy <maria_botros94@hotmail.com>
2023-08-15 12:03:36 -0700

    timextable

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print time tables
+ *
+ * Return: Always return 0 (Success) program stopped
+ */
+int main(void)
+{
+print_times_table(3);
+_putchar('\n');
+print_times_table(5);
+_putchar('\n');
+print_times_table(98);
+_putchar('\n');
+print_times_table(12);
+return (0);
+}
