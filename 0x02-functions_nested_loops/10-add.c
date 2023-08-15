15fd31e9cb66dfa13a3ca00d0ca803d1017c5192
MariaRamzy <maria_botros94@hotmail.com>
2023-08-15 09:38:25 -0700

    add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - Function that adds two integers
+ * @i: An integer i
+ * @j: An integer j
+ * Return: Result to add a and b
+ */
+int add(int i, int j)
+{
+return (i + j);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - add 2 numbers
+ *
+ * Return: Always return 0 (Success) program stopped
+ */
+int main(void)
+{
+int n;
+n = add(89, 9);
+printf("%d\n", n);
+return (0);
+}
