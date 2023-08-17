2dc79cb3470b9017461a2f5f7d32dee5882432bc
MariaRamzy <maria_botros94@hotmail.com>
2023-08-17 05:38:09 -0700

    multiply

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,12 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for characters that are digits
- * @a: character
- * @b: character
- *
- * Return: multiplication result
- */
-
+  * mul - Multiplies two integers
+  * @a: The first number to multiply
+  * @b: The second number to multiply with first number
+  *
+  * Return: The result of multiplication between a param and b param
+  */
 int mul(int a, int b)
 {
 return (a * b);
